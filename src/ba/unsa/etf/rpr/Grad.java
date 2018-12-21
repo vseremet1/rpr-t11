199db47a1afb065fdb6c27383f32c72602c8be4b
vseremet1 <vseremet1@etf.unsa.ba>
2018-12-21 13:59:01 +0100

    Grad

---
+++ src/ba/unsa/etf/rpr/Grad.java
@@ -0,0 +1,43 @@
+package ba.unsa.etf.rpr;
+
+public class Grad {
+
+    int ID;
+    String Naziv;
+    int broj_stanovnika;
+    Drzava d;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNaziv() {
+        return Naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        Naziv = naziv;
+    }
+
+    public int getBroj_stanovnika() {
+        return broj_stanovnika;
+    }
+
+    public void setBroj_stanovnika(int broj_stanovnika) {
+        this.broj_stanovnika = broj_stanovnika;
+    }
+
+    public ba.unsa.etf.rpr.Drzava getDrzava() {
+        return d;
+    }
+
+    public void setD(Drzava d) {
+        this.d = d;
+    }
+
+
+}
