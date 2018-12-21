f8dbb1bc9d657864634a60b53ceb12b0a88cff37
vseremet1 <vseremet1@etf.unsa.ba>
2018-12-21 13:58:40 +0100

    Drzava

---
+++ src/ba/unsa/etf/rpr/Drzava.java
@@ -0,0 +1,34 @@
+package ba.unsa.etf.rpr;
+
+public class Drzava {
+
+
+    int ID;
+    String Naziv;
+    int glavni_grad;
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNaziv() {
+        return Naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        Naziv = naziv;
+    }
+
+    public int getGlavni_grad() {
+        return glavni_grad;
+    }
+
+    public void setGlavni_grad(int glavni_grad) {
+        this.glavni_grad = glavni_grad;
+    }
+}
