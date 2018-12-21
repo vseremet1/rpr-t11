1ed24d838c135458635063630136991cf4601b81
vseremet1 <TgFk07892>
2018-12-21 17:40:26 +0100

    i2

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -9,10 +9,11 @@ public class Main {
 
 
     public static String ispisiGradove() throws SQLException {
-        GeografijaDAO gd = (GeografijaDAO) GeografijaDAO.getInstance();
+        GeografijaDAO gd =GeografijaDAO.getInstance();
+
         ArrayList<Grad> gradovi = gd.gradovi();
 
-        String string = null;
+        String string = "";
 
         for (int i = 0; i < gradovi.size(); i++) {
             string += gradovi.get(i).getNaziv();
@@ -22,24 +23,7 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-
-        Connection connection = null;
-        try {
-            connection = GeografijaDAO.getConnection();
-            if (connection != null) {
-                System.out.println("OK");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        GeografijaDAO dao = GeografijaDAO.getInstance();
 
         System.out.println("Gradovi su:\n" + ispisiGradove());
         glavniGrad();
