b7dadba771b3f711f8b9a87b42cf94b8e27464ba
vseremet1 <vseremet1@etf.unsa.ba>
2018-12-25 13:20:58 +0100

    main

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,11 +1,18 @@
 package ba.unsa.etf.rpr;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+import static javafx.application.Application.launch;
+
+public class Main  {
 
 
     public static String ispisiGradove() throws SQLException {
@@ -16,17 +23,28 @@ public class Main {
         String string = "";
 
         for (int i = 0; i < gradovi.size(); i++) {
-            string += gradovi.get(i).getNaziv();
+            string += gradovi.get(i).getNaziv()+ " ";
         }
         return string;
     }
 
+
+     public void start(Stage primaryStage) throws Exception{
+
+        GeografijaDAO model=GeografijaDAO.getInstance();
+
+        FXMLLoader loader = new FXMLLoader( getClass().getResource("gradovi.fxml" ));
+        loader.setController(new Viewer(model));
+        Parent root=loader.load();
+        primaryStage.setTitle("Gradovi");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
     public static void main(String[] args) throws SQLException {
-
         GeografijaDAO dao = GeografijaDAO.getInstance();
-
         System.out.println("Gradovi su:\n" + ispisiGradove());
         glavniGrad();
+        launch(args);
 
     }
 
@@ -34,7 +52,7 @@ public class Main {
         System.out.println("Unesite ime drzave: ");
         Scanner ulaz = new Scanner(System.in);
 
-        String drz = ulaz.nextLine();
+        String drzava = ulaz.nextLine();
 
     }
 }
